7c0f5ce8015d55311c800cf660e67a5bfc5becba
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-12-13 09:22:47 +0100

    added container component

---
+++ addon/components/comite-aanstelling/aanstelling-container.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/comite-aanstelling/aanstelling-container';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  layout,
+
+  didReceiveAttrs(){
+    this._super(...arguments);
+    this.set('artikel', 1);
+  },
+
+  actions: {
+
+    getArtikel(number){
+      this.set('artikel', number);
+    }
+  }
+
+});
